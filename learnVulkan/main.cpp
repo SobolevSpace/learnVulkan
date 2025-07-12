9eb0e146bd9adc251bd44f9b99392b0ecdd35ca8
wanchengli <wanchengli@tencent.com>
2025-07-12 11:29:03 +0800

    initialize environments

---
+++ learnVulkan/main.cpp
@@ -0,0 +1,62 @@
+#include "vulkan/vulkan.h"
+#define GLFW_INCLUDE_VULKAN
+#include "GLFW/glfw3.h"
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+constexpr uint32_t WIDTH = 800;
+constexpr uint32_t HEIGHT = 600;
+
+class HelloTriangleApplication {
+public:
+
+	void run() {
+		initWindow();
+		initVulkan();
+		mainLoop();
+		cleanUp();
+	}
+
+private:
+	GLFWwindow* window = nullptr;
+
+	void initWindow() {
+		glfwInit();
+		
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+		window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+	}
+
+	void initVulkan() {
+		;
+	}
+
+	void mainLoop() {
+		while (!glfwWindowShouldClose(window)) {
+			glfwPollEvents();
+		}
+	}
+
+	void cleanUp() {
+		glfwDestroyWindow(window);
+		glfwTerminate();
+	}
+};
+
+int main() {
+	HelloTriangleApplication app;
+
+	try {
+		app.run();
+	}
+	catch (const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+}
